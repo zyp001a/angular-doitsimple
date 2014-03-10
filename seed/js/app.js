bc4db94f5f68428f18a94899165a8cd148d0f67b
zyp001a <yzhou01a@gmail.com>
2014-03-10 23:13:36 +0800

    middle version

--- seed/js/app.js
+++ seed/js/app.js
@@ -1,7 +1,23 @@
-var bookApp = angular.module('bookApp',['eveCore', 'eveHeaderController']);
+var bookApp = angular.module('bookApp',
+	[
+		'eveCore', 
+		'eveHeaderController', 
+		'eveFileUpload'
+	]);
 bookApp.run(function(Config){
-	Config.setDirRoot("seed");
-	Config.setRestRoot("/books");
+	Config.dirRoot = "seed";
+	Config.restRoot = "/books";
+	Config.defaultTplRoot = "src/tpl";
+//	Config.templateUrls.list = "seed/partials/list.html";
+//	Config.templateUrls.modify = "seed/partials/modify.html";
 });
 
-
+bookApp.directive('myCustomer', function() {
+      return {
+        restrict: 'E',
+        scope: {
+          customerInfo: '=info'
+        },
+        template: ' Name: {{customerInfo.name}} Address: {{customerInfo.address}}'
+      };
+    });
